00fb1c28f49d46bfe31c2defc2e86737483f56de
Sabeel Aijaz <141610619+sabeel28@users.noreply.github.com>
2024-09-07 15:44:32 +0530

    Update main.c

--- TestSab/main.c
+++ TestSab/main.c
@@ -7,14 +7,15 @@ int main()
 {
     double width = 23;
     double height = 45;
-    double area = 0;
-    double perimeter = 0;
+    // double area = 0;
+    // double perimeter = 0;
+    // perimeter = 2*(height+width);
+    // area = width*height;
+    double perimeter = 2*(height+width);
+    double area = width*height;
 
-    perimeter = 2*(height+width);
-    area = width*height;
-
-    printf("Perimeter is : %f\n",perimeter);
-    printf("area is : %f\t",area);
+    printf("Perimeter is : %0.2f\n",perimeter);
+    printf("area is : %0.2f\t",area);
 
     return 0;
 
